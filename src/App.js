a658eb81d39b04e4edb48e0cf0d480b64efaf708
NabilRaiyan <rraiyan77@gmail.com>
2024-01-14 11:34:36 +0600

    Adding links and navigation functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import {Routes, Route} from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
 
-
+const Shop  = () =>{
+  return (
+    <div>
+      <h1>SHOP PAGE</h1>
+    </div>
+  )
+}
 
 
 const App = ()=> {
@@ -11,6 +17,7 @@ const App = ()=> {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
+        <Route path='/shop' element={<Shop />}></Route>
       </Route>
     </Routes>
     
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
 
 const Navigation = ()=>{
     return (
-      <div>
-        I am the navigation bar
+      <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to='/'>
+                <div>Logo</div>
+            </Link>
+            
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
         <Outlet />
-      </div>
+      </Fragment>
     );
   }
 
