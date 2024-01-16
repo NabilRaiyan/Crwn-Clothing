8291ab3fe47edc7d139070de80a924148329cdba
NabilRaiyan <rraiyan77@gmail.com>
2024-01-16 12:08:35 +0600

    Commit firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop  = () =>{
   return (
@@ -18,6 +19,7 @@ const App = ()=> {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
         <Route path='/shop' element={<Shop />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
       </Route>
     </Routes>
     
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,12 @@
+
+
+
+const SignIn = ()=>{
+    return(
+        <div>
+            Sign in page
+        </div>
+    )
+}
+
+export default SignIn;
