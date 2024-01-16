a6446936fa6939806b922ecbabfd9a4f421b73f5
NabilRaiyan <rraiyan77@gmail.com>
2024-01-16 20:35:33 +0600

    Adding firebase auth

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app';
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCl-kpxFMfeg8gzlJ5sE3WLDzXRCvkTXlU",
+    authDomain: "crwn-clothing-db-aa551.firebaseapp.com",
+    projectId: "crwn-clothing-db-aa551",
+    storageBucket: "crwn-clothing-db-aa551.appspot.com",
+    messagingSenderId: "26261116112",
+    appId: "1:26261116112:web:fa0ceda0dac98210e57ad5"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
