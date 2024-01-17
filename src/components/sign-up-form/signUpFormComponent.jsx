9fdc899eec4c9d3598e88361436b0d219e39ea2f
NabilRaiyan <rraiyan77@gmail.com>
2024-01-17 10:51:53 +0600

    Adding sign up form

--- src/components/sign-up-form/signUpFormComponent.jsx
+++ src/components/sign-up-form/signUpFormComponent.jsx
@@ -4,18 +4,21 @@ const SignUp = ()=>{
 
     return(
         <div>
-            <h1>Sign up form using email and password</h1>
-            <label>Username</label>
-            <input placeholder = 'e.g Raiyan' type="text" required/>
+           <form onSubmit={()=>{}}>
+                <h1>Sign up form using email and password</h1>
+                <label>Username</label>
+                <input placeholder = 'e.g Raiyan' type="text" required/>
 
-            <label>Email</label>
-            <input placeholder="e.g raiyan7@gmail.com" type="email" required/>
+                <label>Email</label>
+                <input placeholder="e.g raiyan7@gmail.com" type="email" required/>
 
-            <label>Password</label>
-            <input type="password" required/>
+                <label>Password</label>
+                <input type="password" required/>
 
-            <label>Confirm Password</label>
-            <input type="password" required/>
+                <label>Confirm Password</label>
+                <input type="password" required/>
+                <button type="submit">SIGN UP</button>
+           </form>
         </div>
     );
 }
