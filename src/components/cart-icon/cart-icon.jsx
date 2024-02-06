bbc22e8015fa7f5c6f48b3b0a216558626402851
NabilRaiyan <rraiyan77@gmail.com>
2024-02-06 09:42:28 +0600

    Organizing code

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as ShoopingIcon } from '../../assets/shopping-bag.svg';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
-
+// creating cart icon
 const CartIcon = ()=>{
     const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
     const toggleIsCartOpen = ()=>{
