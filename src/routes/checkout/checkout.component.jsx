f8c942abc5e67aa1e46a3fabc6f11f584cf00df0
NabilRaiyan <rraiyan77@gmail.com>
2024-02-14 22:22:52 +0600

    Adding styles

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,32 +1,49 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
+import "./checkout.styles.scss";
+import "./checkout-item.styles.scss";
 // funtion for checkout functionality
 const CHECKOUT = ()=>{
     const {cartItems, addItemToCart, removeItemToCart} = useContext(CartContext)
     return(
-        <div>
-            <h1>CHECKOUT PAGE</h1>
-            <div>
-            {   // getting items form list
-                cartItems.map((cartItem)=>{
-                    const {id, name, quantity} = cartItem;
-                    const itemIncreaseHandler = ()=>{
-                        addItemToCart(cartItem)
-                    }
-                    return(
-                    <div  key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span> 
-                        <br />
-                        <span onClick={itemIncreaseHandler}>increment</span>
-                        <br />
-                        <span onClick={()=>removeItemToCart(cartItem)}>decrement</span>
-                    </div>
-                    
-                )})
-            }
-                
+        <div className="checkout-container">
+            <div className="checkout-block">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
             </div>
+        {   
+            // getting items form list
+            cartItems.map((cartItem)=>{
+                const {id, name, quantity} = cartItem;
+                const itemIncreaseHandler = ()=>{
+                    addItemToCart(cartItem)
+                }
+                return(
+                <div  key={id}>
+                    <h2>{name}</h2>
+                    <span>{quantity}</span> 
+                    <br />
+                    <span onClick={itemIncreaseHandler}>increment</span>
+                    <br />
+                    <span onClick={()=>removeItemToCart(cartItem)}>decrement</span>
+                </div>
+                
+            )})
+        } 
+        <span className="Total">Total: </span>
         </div>
     )
 }
