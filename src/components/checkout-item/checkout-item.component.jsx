1f16336c4fd1793adadd3d5104d078bf14581b59
NabilRaiyan <rraiyan77@gmail.com>
2024-02-15 15:01:28 +0600

    Adding increment and decrement arrow for checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -16,7 +16,15 @@ const CheckoutItem = ({cartItem})=>{
                 <img src={imageUrl} alt={`${name}`}/>
             </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div className="arrow">
+                &#10094;
+                </div>
+                <span className="value">{quantity}</span>
+                <div className="arrow">
+                    &#10095;
+                </div>
+                </span>
             <span className="price">{price}</span>
             <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
         
