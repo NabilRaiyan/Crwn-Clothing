8d2943dd4bf1171a172060c66381e3315e317f4b
NabilRaiyan <rraiyan77@gmail.com>
2024-02-15 19:25:12 +0600

    adding data to the firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import PRODUCTS from '../shop-data.json';
+import SHOP_DATA from '../shop-data.js';
 
 
 export const ProductsContext = createContext({
@@ -8,7 +8,7 @@ export const ProductsContext = createContext({
 
 
 export const ProductsProvider = ({children})=>{
-    const [ products, setProducts ] = useState(PRODUCTS);
+    const [ products, setProducts ] = useState([]);
     const value = {products};
     return(
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
