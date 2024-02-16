339c53ae82bbbf7093fd0080a183b16944980192
NabilRaiyan <rraiyan77@gmail.com>
2024-02-16 19:35:34 +0600

    adding data to the db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import SHOP_DATA from '../shop-data.js';
+// import SHOP_DATA from '../shop-data.js';
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 
